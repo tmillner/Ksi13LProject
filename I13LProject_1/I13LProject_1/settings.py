fdce0d076b9b3f11db5f9665ba3e360af99c7150
Trevor Millner <trevormillner@gmail.com>
2014-03-30 06:50:20 -0400

    Done hacking, had to tidy up settings.py and install dependencies for apps. This is for removing crash error

--- I13LProject_1/I13LProject_1/settings.py
+++ I13LProject_1/I13LProject_1/settings.py
@@ -1,7 +1,7 @@
 # Django settings for I13LProject_1 project.
 
 DEBUG = False
-TEMPLATE_DEBUG = DEBUG
+#TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
@@ -23,7 +23,8 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO','https')
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['*']
+#ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['young-reaches-5569.herokuapp.com']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_Qby_name
@@ -63,7 +64,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = 'staticfiles'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
